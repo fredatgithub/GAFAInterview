f067ad7338fefbdb9b2038c2a2a32b33ebc0f68e
Fred <domaine75@gmail.com>
2017-09-11 21:11:48 +0200

    Initial commit

---
+++ InterviewAlgorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewAlgorithms
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      /*
+       Problems in text file 
+       * */
+
+      Display("press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
