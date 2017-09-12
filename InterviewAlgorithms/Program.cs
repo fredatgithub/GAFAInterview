c530065622dff696b924f80908babc86f607a509
Fred <domaine75@gmail.com>
2017-09-12 13:10:02 +0200

    Add problem 1

--- InterviewAlgorithms/Program.cs
+++ InterviewAlgorithms/Program.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 
 namespace InterviewAlgorithms
 {
-  class Program
+  internal static class Program
   {
-    static void Main()
+    private static void Main()
     {
       Action<string> Display = Console.WriteLine;
+      /* Problems are in text file  */
+      // problem 1 Netflix
       /*
-       Problems in text file 
+       Netflix
+        =======
+        1. Given a file full of these strings:
+        Vivek 1
+        Vivek 2
+        Ajit 3
+        Krishna 4
+        Keshav 6
+        Keshav 7
+        Print the duplicates and the IDs:
+        Vivek: 1, 2
+        Keshav: 6, 7
        * */
+
+      List<string> list1 = new List<string>
+      {
+        "Vivek 1",
+        "Vivek 2",
+        "Ajit 3",
+        "Krishna 4",
+        "Keshav 6",
+        "Keshav 7"
+      };
+
+      foreach (string item in list1)
+      {
+        
+      }
 
       Display("press any key to exit:");
       Console.ReadKey();
