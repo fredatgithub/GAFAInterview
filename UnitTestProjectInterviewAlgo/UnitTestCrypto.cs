0c8845a1c47a4103085c63984f291020524465fb
Fred <domaine75@gmail.com>
2017-10-22 00:24:44 +0200

    Add unit test

---
+++ UnitTestProjectInterviewAlgo/UnitTestCrypto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo = InterviewAlgorithms.Program;
+
+
+namespace UnitTestProjectInterviewAlgo
+{
+  [TestClass]
+  public class UnitTestCrypto
+  {
+    [TestMethod]
+    public void TestMethod_GetRandomChar()
+    {
+      string source = string.Empty;
+      string expected = string.Empty;
+      //List<char> result = Algo.GetRandomChar(source);
+      Assert.IsTrue(true); // result.count > 1
+    }
+  }
+}
